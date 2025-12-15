99d0b1e2a1badc9b508b78c695667049e8acbfd5
abdulnahid-aws <abdul.nahid@gmail.com>
2025-12-15 18:42:19 +0530

    py_basic_variables

---
+++ basic_variables.py
@@ -0,0 +1,8 @@
+x=100
+print(type(x))
+name="John"
+print(type(name))
+price=19.99
+print(type(price))
+is_student=True
+print(type(is_student))
--- data_types_python.py
+++
@@ -1,3 +0,0 @@
-year_of_birth = input("Enter your year of birth: ")
-age= 2025 - int(year_of_birth)
-print("You will be", age, "years old in 2025.")
